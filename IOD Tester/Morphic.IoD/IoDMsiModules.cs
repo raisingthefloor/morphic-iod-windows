6b44182a16ee53db57f1434cd25b6ae85f943a95
James Vanderheiden <james.v@digipen.edu>
2021-02-09 17:16:29 -0800

    progress monitoring for MSIs

--- IOD Tester/Morphic.IoD/IoDMsiModules.cs
+++ IOD Tester/Morphic.IoD/IoDMsiModules.cs
@@ -23,7 +23,7 @@ namespace Morphic.IoD
             status = IoDStatus.OK;
             try
             {
-                Installer.SetInternalUI(InstallUIOptions.UacOnly);
+                Installer.SetInternalUI(InstallUIOptions.Silent);
                 Installer.SetExternalUI(handler,
                     InstallLogModes.ActionData |
                     InstallLogModes.ActionStart |
@@ -36,7 +36,7 @@ namespace Morphic.IoD
                     InstallLogModes.Initialize |
                     InstallLogModes.OutOfDiskSpace |
                     InstallLogModes.Progress |
-                    //InstallLogModes.PropertyDump |
+                    InstallLogModes.PropertyDump |
                     InstallLogModes.ResolveSource |
                     InstallLogModes.RMFilesInUse |
                     InstallLogModes.ShowDialog |
@@ -86,7 +86,7 @@ namespace Morphic.IoD
             status = IoDStatus.OK;
             try
             {
-                Installer.SetInternalUI(InstallUIOptions.UacOnly);
+                Installer.SetInternalUI(InstallUIOptions.Silent);
                 Installer.SetExternalUI(handler,
                     InstallLogModes.ActionData |
                     InstallLogModes.ActionStart |
@@ -99,7 +99,7 @@ namespace Morphic.IoD
                     InstallLogModes.Initialize |
                     InstallLogModes.OutOfDiskSpace |
                     InstallLogModes.Progress |
-                    //InstallLogModes.PropertyDump |
+                    InstallLogModes.PropertyDump |
                     InstallLogModes.ResolveSource |
                     InstallLogModes.RMFilesInUse |
                     InstallLogModes.ShowDialog |
@@ -144,28 +144,51 @@ namespace Morphic.IoD
 
         private bool active;
         private string msiFilepath;
-        private ExternalUIHandler handler;
+        private ExternalUIRecordHandler handler;
         private IoDStatus status;
 
-        MessageResult UIMonitor(InstallMessage messageType, string message, MessageButtons buttons, MessageIcon icon, MessageDefaultButton defaultButton)
+        public double progressValue;
+        private int progressTotal;
+        private int progressCompleted;
+        private int progressStep;
+        private bool progressMove;
+        private bool enableActionData;
+        private int progressPhase;
+        private double progressWeight = 0.5;
+
+        MessageResult UIMonitor(InstallMessage messageType, Record messageRecord, MessageButtons buttons, MessageIcon icon, MessageDefaultButton defaultButton)
         {
             MessageResult result = new MessageResult();
             switch (messageType)
             {
                 case InstallMessage.ActionData:
-
+                    if(this.enableActionData)
+                    {
+                        if(this.progressMove)
+                        {
+                            this.progressCompleted += this.progressStep;
+                        }
+                        else
+                        {
+                            this.progressCompleted -= this.progressStep;
+                        }
+                        this.updateProgress();
+                    }
                     break;
                 case InstallMessage.ActionStart:
-
+                    if(this.enableActionData)
+                    {
+                        this.enableActionData = false;
+                    }
                     break;
                 case InstallMessage.CommonData:
 
                     break;
                 case InstallMessage.Error:
-                    Console.WriteLine(message);
+                    Console.WriteLine(messageRecord.ToString());
                     break;
                 case InstallMessage.FatalExit:
-                    Console.WriteLine(message);
+                    Console.WriteLine(messageRecord.ToString());
                     if (status <= IoDStatus.MiscFailure) status = IoDStatus.MiscFailure;
                     break;
                 case InstallMessage.FilesInUse:
@@ -176,20 +199,20 @@ namespace Morphic.IoD
 
                     break;
                 case InstallMessage.Initialize:
-                    Console.WriteLine("Initializing");
+
                     break;
                 case InstallMessage.InstallEnd:
-                    Console.WriteLine("Ending installation");
+
                     break;
                 case InstallMessage.InstallStart:
-                    Console.WriteLine("Beginning installation");
+
                     break;
                 case InstallMessage.OutOfDiskSpace:
                     Console.WriteLine("Insufficient disk space");
                     if (status <= IoDStatus.MiscFailure) status = IoDStatus.NoSpace;
                     break;
                 case InstallMessage.Progress:
-                    if (message != "" && verbose) Console.WriteLine(message);
+                    TrackProgress(messageRecord);
                     break;
                 case InstallMessage.ResolveSource:
 
@@ -207,10 +230,100 @@ namespace Morphic.IoD
 
                     break;
                 case InstallMessage.Warning:
-                    Console.WriteLine(message);
+                    Console.WriteLine(messageRecord.ToString());
                     break;
             }
             return result;
+        }
+
+        private void TrackProgress(Record messageRecord)
+        {
+            if (messageRecord == null || messageRecord.FieldCount == 0)
+            {
+                return;
+            }
+
+            int fieldCount = messageRecord.FieldCount;
+            int progressType = messageRecord.GetInteger(1);
+            string progressTypeString = String.Empty;
+            switch (progressType)
+            {
+                case 0: //progress reset
+                    if (fieldCount < 4)
+                    {
+                        return;
+                    }
+                    this.progressPhase++;
+                    this.progressTotal = messageRecord.GetInteger(2);
+                    if (this.progressPhase == 1)
+                    {
+                        //SHENANIGANS by Installer team
+                        this.progressTotal += 50;
+                    }
+                    this.progressMove = (messageRecord.GetInteger(3) == 0);
+                    this.progressCompleted = (this.progressMove ? 0 : this.progressTotal);
+                    this.enableActionData = false;
+                    this.updateProgress();
+                    break;
+                case 1:
+                    if (fieldCount < 3)
+                    {
+                        return;
+                    }
+                    if (messageRecord.GetInteger(3) == 0)
+                    {
+                        this.enableActionData = false;
+                    }
+                    else
+                    {
+                        this.enableActionData = true;
+                        this.progressStep = messageRecord.GetInteger(2);
+                    }
+                    break;
+                case 2:
+                    if (fieldCount < 2 || this.progressTotal == 0 || this.progressPhase == 0)
+                    {
+                        return;
+                    }
+                    if (this.progressMove)
+                    {
+                        this.progressCompleted += messageRecord.GetInteger(2);
+                    }
+                    else
+                    {
+                        this.progressTotal -= messageRecord.GetInteger(2);
+                    }
+                    this.updateProgress();
+                    break;
+                case 3:
+                    this.progressTotal += messageRecord.GetInteger(2);
+                    break;
+            }
+        }
+
+        private void updateProgress()
+        {
+            if (this.progressPhase < 1 || this.progressTotal == 0)
+            {
+                this.progressValue = 0;
+            }
+            else if (this.progressPhase == 1)
+            {
+                this.progressValue = this.progressWeight * Math.Min(this.progressCompleted, this.progressTotal) / this.progressTotal;
+            }
+            else if (this.progressPhase == 2)
+            {
+                this.progressValue = this.progressWeight + (1 - this.progressWeight) * Math.Min(this.progressCompleted, this.progressTotal) / this.progressTotal;
+            }
+            else
+            {
+                this.progressValue = 1;
+            }
+
+            if (this.verbose)
+            {
+                Console.WriteLine((100 * this.progressValue).ToString() + "%");
+            }
         }
     }
 }
